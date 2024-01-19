e4c9dc8b9caff654174b4ed7fbc3b3554ac09c1b
marco-polverini-dev <128422361+marco-polverini-dev@users.noreply.github.com>
2024-01-19 20:27:47 +0100

    refactor: exception handler + validation error message (#10)

--- src/main/java/it/magiavventure/common/error/handler/DefaultExceptionHandler.java
+++ src/main/java/it/magiavventure/common/error/handler/DefaultExceptionHandler.java
@@ -8,26 +8,54 @@ import it.magiavventure.common.model.Error;
 import it.magiavventure.common.model.HttpError;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @AllArgsConstructor
-public class DefaultExceptionHandler {
+public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
     private final CommonProperties commonProperties;
     private final HttpErrorMapper httpErrorMapper;
 
     @ExceptionHandler({MagiavventureException.class})
-    public ResponseEntity<HttpError> exceptionHandler(MagiavventureException magiavventureException) {
+    public ResponseEntity<HttpError> handleException(MagiavventureException magiavventureException) {
 
         Error error = magiavventureException.getError();
         ErrorMessage errorMessage = retrieveError(error.getKey());
 
         HttpError httpError = httpErrorMapper.map(errorMessage);
         httpError.setMessage(formatMessage(errorMessage.getMessage(), error.getArgs()));
+
+        return ResponseEntity
+                .status(httpError.getStatus())
+                .body(httpError);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
+                                                                  @NonNull HttpHeaders headers,
+                                                                  @NonNull HttpStatusCode status,
+                                                                  @NonNull WebRequest request) {
+        ErrorMessage errorMessage = retrieveError(MagiavventureException.VALIDATION_ERROR);
+
+        List<String> fields = new ArrayList<>();
+        exception.getBindingResult().getFieldErrors().forEach(fieldError -> fields.add(fieldError.getField()));
+        HttpError httpError = httpErrorMapper.map(errorMessage);
+        httpError.setFields(fields);
 
         return ResponseEntity
                 .status(httpError.getStatus())
--- src/main/java/it/magiavventure/common/model/HttpError.java
+++ src/main/java/it/magiavventure/common/model/HttpError.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -16,4 +18,5 @@ public class HttpError {
     private int status;
     private String message;
     private String description;
+    private List<String> fields;
 }
